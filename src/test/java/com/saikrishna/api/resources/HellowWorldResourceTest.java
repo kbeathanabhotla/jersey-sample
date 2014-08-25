7416de192af78dd9420e2ba2b9d6b22ea60714b9
saikrishna-kishore <krishna.sai6491@gmail.com>
2014-08-26 04:47:16 +0530

    added configuration related to JTF and updated Sample TestCase

--- src/test/java/com/saikrishna/api/resources/HellowWorldResourceTest.java
+++ src/test/java/com/saikrishna/api/resources/HellowWorldResourceTest.java
@@ -2,13 +2,35 @@ package com.saikrishna.api.resources;
 
 import static org.junit.Assert.*;
 
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class HellowWorldResourceTest {
+import com.saikrishna.api.configuration.SpringContextJerseyTest;
+import com.saikrishna.api.configuration.WebAppResourceConfig;
 
-	@Test
-	public void testGetMessage() {
-		fail("Not yet implemented");
+public class HellowWorldResourceTest extends SpringContextJerseyTest {
+	
+	@Override
+	protected Application configure() {
+		ResourceConfig resourceConfig = new WebAppResourceConfig();
+		enable(TestProperties.LOG_TRAFFIC);
+		enable(TestProperties.DUMP_ENTITY);
+		resourceConfig.property("contextConfigLocation",
+				"classpath:applicationContext.xml");
+		return resourceConfig;
 	}
 
+	@Test
+	public void test_get_message() {
+
+		Response response = target("hello").request().get(Response.class);
+		Assert.assertEquals(200, response.getStatus());
+		//Assert.assertEquals("Hello Jersye and spring world", response.getEntity());
+
+	}
 }
